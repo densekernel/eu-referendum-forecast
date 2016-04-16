1c3705b7dfad900537fb1a6954e4f3f87dec38a9
Gabriel Bila <mielunegru@gmail.com>
2016-04-16 17:54:09 +0100

    Generated heatmap for 'leave' and 'stay' tweets. Ability to toggle between 'stay' and 'leave' heatmap. Also integrated real data from pickled file from Jonny

--- app/geo.py
+++ app/geo.py
@@ -1,55 +1,87 @@
 import json
-import random
-
-import pandas as pd
+import pickle
 
 topics = ['stay', 'leave']
 sentiments = [0, 4]  # 0 for negative, 4 for positive
 
 
-# read DataFrame containing tweet location info, topic and sentiment toward that topic
 def read_tweets():
-    data_list = ['../data/tweets_line/2016-04-' + str(x) + '.json' for x in
-                 ['03', '04', '05', '06', '07', '08', '09', '10', '11', '12']]
-    # data_list = ['../data/tweets_line/2016-04-' + str(x) + '.json' for x in ['10', '11']]
-    tweet_list = []
-
-    for day_file in data_list:
-        with open(day_file, 'r') as f:
-            print 'processing file ' + day_file
-            for line in f:
-                tweet = json.loads(line, encoding='latin-1')
-                # filtering tweets with some location data included
-                if tweet['place'] or tweet['user']['location']:
-                    tweet_list.append({
-                        'place': tweet['place'],
-                        'userLocation': tweet['user']['location'],
-                        # TODO add columns for stay/leave and sentiment (pos/neg) once Jonny creates them.
-                        # TODO For now, assigns random values to these fields.
-                        'topic': random.choice(topics),
-                        'sentiment': random.choice(sentiments)
-                    })
-
-    df = pd.DataFrame(tweet_list)
-    print len(df.index)
+    print "Reading tweets from pkl file"
+    with open('../data/brexit.pkl', 'rb') as f:
+        df = pickle.load(f)
+    print "Got the tweets"
     return df
 
+# read DataFrame containing tweet location info, topic and sentiment toward that topic
+# def read_tweets():
+#     data_list = ['../data/tweets_line/2016-04-' + str(x) + '.json' for x in
+#                  ['03', '04', '05', '06', '07', '08', '09', '10', '11', '12']]
+#     # data_list = ['../data/tweets_line/2016-04-' + str(x) + '.json' for x in ['10', '11']]
+#     tweet_list = []
+#
+#     for day_file in data_list:
+#         with open(day_file, 'r') as f:
+#             print 'processing file ' + day_file
+#             for line in f:
+#                 tweet = json.loads(line, encoding='latin-1')
+#                 # filtering tweets with some location data included
+#                 if tweet['place'] or tweet['user']['location']:
+#                     tweet_list.append({
+#                         'place': tweet['place'],
+#                         'userLocation': tweet['user']['location'],
+#                         # TODO add columns for stay/leave and sentiment (pos/neg) once Jonny creates them.
+#                         # TODO For now, assigns random values to these fields.
+#                         'topic': random.choice(topics),
+#                         'sentiment': random.choice(sentiments)
+#                     })
+#
+#     df = pd.DataFrame(tweet_list)
+#     print len(df.index)
+#     return df
 
-def map_tweet(tweet, location_coord_dict):
+
+def tweet_coords(tweet, location_coord_dict):
     try:
         bounding_box = tweet['place']['boundingBoxCoordinates'][0][0]
         coordinates = [bounding_box['latitude'], bounding_box['longitude']]
     except:
         # filter out generic locations, even if they have coordinates in the geolocation dict.
-        if tweet['userLocation'] in ['United Kingdom', 'UK', 'England', 'London']:
+        if tweet['user']['location'] in ['United Kingdom', 'UK', 'England', 'London']:
             coordinates = None
         else:
-            coordinates = location_coord_dict.get(tweet['userLocation'], None)
+            coordinates = location_coord_dict.get(tweet['user']['location'], None)
+    return coordinates
 
+
+def is_pro_leave(tweet):
+    # positive towards 'leave'
+    if tweet['subject'] == 'leave' and tweet['sentiment'] == 'pos':
+        return True
+    # negative towards 'stay'
+    if tweet['subject'] == 'stay' and tweet['sentiment'] == 'neg':
+        return True
+    # otherwise, it's pro stay or neutral
+    return False
+
+
+def is_pro_stay(tweet):
+    # positive towards 'stay'
+    if tweet['subject'] == 'stay' and tweet['sentiment'] == 'pos':
+        return True
+    # negative towards 'leave'
+    if tweet['subject'] == 'leave' and tweet['sentiment'] == 'neg':
+        return True
+    # otherwise, it's pro leave or neutral
+    return False
+
+
+def map_tweet(tweet, location_coord_dict):
     return {
-        'coordinates': coordinates,
-        'topic': tweet['topic'],
-        'sentiment': tweet['sentiment']
+        'coordinates': tweet_coords(tweet, location_coord_dict),
+        'subject': tweet['subject_strict'],
+        'sentiment': tweet['sentiment'],
+        'pro_leave': is_pro_leave(tweet),
+        'pro_stay': is_pro_stay(tweet)
     }
 
 
@@ -62,22 +94,12 @@ def map_tweets_to_coordinates(df, geolocation_dict):
         coordinate_labeled_tweets.append(map_tweet(tweet, geolocation_dict))
 
     coordinate_labeled_tweets = filter(is_legit_tweet, coordinate_labeled_tweets)
+    print 'tweets with coordinates: ' + str(len(coordinate_labeled_tweets))
     return coordinate_labeled_tweets
 
 
 def is_legit_tweet(coord_tweet):
     return coord_tweet['coordinates'] != None and -12 < coord_tweet['coordinates'][1] < 2.5
-
-
-def is_pro_leave(tweet):
-    # positive towards 'leave'
-    if tweet['topic'] == 'leave' and tweet['sentiment'] == 4:
-        return True
-    # negative towards 'stay'
-    if tweet['topic'] == 'stay' and tweet['sentiment'] == 0:
-        return True
-    # otherwise, it's pro stay
-    return False
 
 
 def gen_geo_data(tweets_with_locations):
@@ -100,7 +122,8 @@ def gen_geo_data(tweets_with_locations):
             'properties': {
                 'text': '',
                 'created_at': '',
-                'vote': 'leave' if is_pro_leave(tweet) else 'remain'
+                'pro_leave': tweet['pro_leave'],
+                'pro_stay': tweet['pro_stay']
             }
         }
 
